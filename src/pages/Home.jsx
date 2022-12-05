13d3183855adbdc2c0357a5c98b250268b9ee02a
Céline Poinson <cpoinson@hotmail.fr>
2022-12-05 12:09:07 +0100

    csssss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <body className="homePageBackground"></body>;
+  return (
+    <body className="homePageBackground">
+      <div className="homePageDiv">
+        <div className="homePageLinksDivs">
+          <button className="formBtn">
+            <Link className="homePageLinks" to="/allBands">
+              Looking for a band?
+            </Link>
+          </button>
+        </div>
+        <div className="homePageLinksDivs">
+          <button className="formBtn">
+            <Link className="homePageLinks" to="/allMusicians">
+              Looking for a musician?
+            </Link>
+          </button>
+        </div>
+      </div>
+    </body>
+  );
 };
 
 export default Home;
