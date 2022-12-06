9cc68653742099263c3c8ccb7b781b8fb436b1de
Céline Poinson <cpoinson@hotmail.fr>
2022-12-06 17:51:27 +0100

    add card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Card = ({ _id, user, ...rest }) => {
+  const data = Object.entries(rest).map((microArray) => {
+    const key = splitWords(microArray[0]);
+    return [key, microArray[1]];
+  });
+  console.log(data);
+  return (
+    <div className="onePostDiv" key={_id}>
+      <div className="imgLinkDiv">
+        <Link to={`${_id}`}>
+          <img className="profilePic" src={user.picture} alt="" />
+        </Link>
+      </div>
+      <p className="onePostName">{user.name}</p>
+      <div className="onePostInfo">
+        {data.map((element) => {
+          return (
+            <p>
+              {element[0]}: {element[1]}
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
+
+function splitWords(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase()) {
+      return `${str.at(0).toUpperCase()}${str.slice(1, i)} ${str
+        .slice(i)
+        .toLowerCase()}`;
+    }
+  }
+  return str.at(0).toUpperCase() + str.slice(1);
+}
